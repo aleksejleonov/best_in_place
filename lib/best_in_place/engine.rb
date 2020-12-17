ae44058592a14901c0a85d57ea14e5bc4d97d0d4
Oleksii Leonov <mail@aleksejleonov.com>
2020-12-17 19:25:11 +0200

    Fix Rails 6 deprecation warning
    ```
    DEPRECATION WARNING: ActionView::Base instances should be constructed with a lookup context, assignments, and a controller.
    (called from new at .../gems/best_in_place-0e694d5fa49f/lib/best_in_place/railtie.rb:4)
    ```

--- lib/best_in_place/engine.rb
+++ lib/best_in_place/engine.rb
@@ -1,8 +1,10 @@
 module BestInPlace
   class Engine < Rails::Engine
     initializer "setup for rails" do
-      ActionView::Base.send(:include, BestInPlace::BestInPlaceHelpers)
-      ActionController::Base.send(:include, BestInPlace::ControllerExtensions)
+      ActiveSupport.on_load(:action_controller) do
+        ActionView::Base.send(:include, BestInPlace::BestInPlaceHelpers)
+        ActionController::Base.send(:include, BestInPlace::ControllerExtensions)
+      end
     end
   end
 end
